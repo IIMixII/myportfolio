76da6248d00a2ef3673c97d5eaf88fc99bf80c6e
kakxem <paul.antonio.almasi@gmail.com>
2023-06-26 17:15:08 +0200

    feat: Add button to redirect to updated portfolio page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react";
-import { Image } from "react-bootstrap";
+import { useEffect, useState } from "react"
+import { Image } from "react-bootstrap"
 
 //     Style
-import profileImg from "../../images/profile_image.jpg";
 import hoverProfileImg from "../../images/hover_profile_image.png"
-import "./Banner.css";
+import profileImg from "../../images/profile_image.jpg"
+import "./Banner.css"
 
 //     Component
 function Banner(props) {
-  const [userHelpHeight, setUserHelpHeight] = useState(35);
-  const [userHelpVisibility, setUserHelpColor] = useState("white");
-  const [imageBorderRadius, setImageBorderRadius] = useState("5%");
-  const [imageBorderColor, setImageBorderColor] = useState("#121420ab");
+  const [userHelpHeight, setUserHelpHeight] = useState(35)
+  const [userHelpVisibility, setUserHelpColor] = useState("white")
+  const [imageBorderRadius, setImageBorderRadius] = useState("5%")
+  const [imageBorderColor, setImageBorderColor] = useState("#121420ab")
   const [hoverImage, setHoverImage] = useState(false)
 
   useEffect(() => {
-    setUserHelpHeight(props.initialState ? 35 : 0);
-    setUserHelpColor(props.initialState ? "white" : "transparent");
-    setImageBorderColor(props.initialState ? "#121420ab" : "#2f008eab");
-    setImageBorderRadius(props.initialState ? "50%" : "30%");
-    if (props.initialState){
+    setUserHelpHeight(props.initialState ? 35 : 0)
+    setUserHelpColor(props.initialState ? "white" : "transparent")
+    setImageBorderColor(props.initialState ? "#121420ab" : "#2f008eab")
+    setImageBorderRadius(props.initialState ? "50%" : "30%")
+    if (props.initialState) {
       setHoverImage(false)
     }
-  }, [props.initialState]);
+  }, [props.initialState])
 
   return (
     <div className="bannerDiv" onClick={() => props.changeComponent("Home")}>
@@ -35,16 +35,28 @@ function Banner(props) {
             borderColor: imageBorderColor,
           }}
           onMouseEnter={() => {
-            if (!props.initialState){
+            if (!props.initialState) {
               setHoverImage(true)
             }
           }}
-          onMouseLeave={()=>{
-            if (!props.initialState){
+          onMouseLeave={() => {
+            if (!props.initialState) {
               setHoverImage(false)
             }
           }}
         />
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <a className="new-portfolio">New portfolio available!</a>
+        </div>
+
         <h1 className="bannerTitle">
           <strong>Paul Antonio Almasi</strong>
         </h1>
@@ -60,7 +72,7 @@ function Banner(props) {
         Click a section...
       </h3>
     </div>
-  );
+  )
 }
 
-export default Banner;
+export default Banner
